b5746686e449b8ab7bffc25d821ad23a6ca524f5
Glenn Meuth <vortarian@systemicai.com>
2017-03-07 00:13:00 -0700

    Fixed get method

--- dists/slscrypt/index.js
+++ dists/slscrypt/index.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const AWS = require('aws-sdk');
 
-const SECRET_LOCATION = process.env['CRYPT_LOCATION'] || 'file://.serverless-secret.json';
+const SECRET_LOCATION = process.env.CRYPT_LOCATION || 'file://.serverless-secret.json';
 
 const aws = require('aws-sdk')
 
@@ -18,7 +18,7 @@ function s3_data(location, cb) {
   let s3 = new aws.S3({apiVersion: '2006-03-01', signatureVersion: 'v4'});
   s3.getObject({ 'Bucket': bucket, 'Key': key }, function(err, data) {
     if(err) cb(err, data);
-    else cb(err, data.Body)
+    else cb(null, data.Body)
   });
 }
 
@@ -32,20 +32,25 @@ function data(location, cb) {
   }
 }
 
+console.log(SECRET_LOCATION);
 module.exports = {
   get(name) {
     return new Promise((resolve, reject) => {
       data(SECRET_LOCATION, function(err, data) { 
-        const secret = JSON.parse(data);
-        const kms = new AWS.KMS({ region: secret['__slscrypt-region'] });
-        kms.decrypt({
-          CiphertextBlob: new Buffer(secret[name], 'base64'),
-        }, (err, data) => {
-          if (err) {
-            reject(err);
-          }
-          resolve(data.Plaintext.toString('utf-8'));
-        });
+        if(err) {
+          reject(err);
+        } else {
+          const secret = JSON.parse(data);
+          const kms = new AWS.KMS({ region: secret['__slscrypt-region'] });
+          kms.decrypt({
+            CiphertextBlob: new Buffer(secret[name], 'base64'),
+          }, (err, data) => {
+            if (err) {
+              reject(err);
+            }
+            resolve(data.Plaintext.toString('utf-8'));
+          });
+        }
       });
     });
   },
